6616772a1caf90ce2c43171ff40cb12842e5accd
VitaliiBo <boflying1@gmail.com>
2020-03-06 21:05:11 +0300

    to do: test for sort

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'distr'),
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
